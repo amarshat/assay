76887d1f2f2dace3cb04e3a0453fd0593c35fc12
Amar Akshat <amar.akshat@gmail.com>
2026-06-07 13:07:12 +0100

    Isabelle leg: drop out-of-scope ntt stub so the lift loads; Assay session now builds clean
    - model: remove ntt (error-stub broke cryptol-to-isabelle); SAW make saw still exits 0 (re-checked).
    - spec/isabelle/MLDSA_NTT.thy regenerated. 'isabelle build -D spec/isabelle Assay' now Finishes:
      lifted model + integer-level spec type-check under the Cryptol image. NO equivalence proven yet.

--- spec/isabelle/MLDSA_NTT.thy
+++ spec/isabelle/MLDSA_NTT.thy
@@ -3,8 +3,6 @@ imports "Cryptol.Cryptol"
 begin
 
 context includes cryptol_translation_syntax begin
-type_synonym N = "256"
-
 cryptol_definition Q64 :: "[64]" where
 "Q64  \<equiv> 0x7fe001 :: [64]"
 
@@ -26,9 +24,6 @@ cryptol_definition montgomery_reduce :: "([64]) \<Rightarrow> ([32])" where
   let
     t = ((drop`{32,32,Bit} (a *`{[64]} QINV)) : ([32]))
   in (drop`{32,32,Bit} ((a -`{[64]} ((sext64`{} t) *`{[64]} Q64)) >>$`{64,Integer} (32 :: Integer)))"
-
-cryptol_definition ntt :: "([N]Integer) \<Rightarrow> ([N]Integer)" where
-"ntt coeffs \<equiv> error`{[N]Integer,48} (list_to_seq [0x6f :: [8],0x75 :: [8],0x74 :: [8],0x20 :: [8],0x6f :: [8],0x66 :: [8],0x20 :: [8],0x73 :: [8],0x63 :: [8],0x6f :: [8],0x70 :: [8],0x65 :: [8],0x20 :: [8],0x66 :: [8],0x6f :: [8],0x72 :: [8],0x20 :: [8],0x76 :: [8],0x31 :: [8],0x3a :: [8],0x20 :: [8],0x66 :: [8],0x6f :: [8],0x72 :: [8],0x77 :: [8],0x61 :: [8],0x72 :: [8],0x64 :: [8],0x20 :: [8],0x4e :: [8],0x54 :: [8],0x54 :: [8],0x20 :: [8],0x6e :: [8],0x6f :: [8],0x74 :: [8],0x20 :: [8],0x6d :: [8],0x6f :: [8],0x64 :: [8],0x65 :: [8],0x6c :: [8],0x65 :: [8],0x64 :: [8],0x20 :: [8],0x79 :: [8],0x65 :: [8],0x74 :: [8]] :: [48][8])"
 
 cryptol_definition q :: "Integer" where
 "q  \<equiv> 8380417 :: Integer"
